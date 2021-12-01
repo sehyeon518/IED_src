1574eb0b0d3dece30335626f64b4c5a947d617d6
sehyeon518 <84698896+sehyeon518@users.noreply.github.com>
2021-12-01 23:22:27 +0900

    Update 22_example_1.ino

--- 22_example_1.ino
+++ 22_example_1.ino
@@ -8,36 +8,18 @@
 #define _DIST_MIN 100
 #define _DIST_MAX 410
 
-#define _DIST_ALPHA 0.1
-
 #define _DUTY_MIN 553
 #define _DUTY_NEU 1476
 #define _DUTY_MAX 2399
-
-#define _SERVO_ANGLE 30
-#define _SERVO_SPEED 30
 
 #define _INTERVAL_DIST 20
 #define _INTERVAL_SERVO 20
 #define _INTERVAL_SERIAL 100
 
-#define _KP 0.1
-
 Servo myservo;
 
-float dist_target;
 float dist_raw, dist_ema, dist_cali;
 
-unsigned long last_sampling_time_dist, last_sampling_time_servo, last_sampling_time_serial;
-bool event_dist, event_servo, event_serial;
-
-int duty_chg_per_interval;
-int duty_target, duty_curr;
-
-float error_curr, error_prev, control, pterm, dterm, iterm;
-
-
-int a, b; // unit : mm
 
 void setup() {
   pinMode(PIN_LED, OUTPUT);
@@ -45,9 +27,6 @@ void setup() {
   Serial.begin(57600);
 
   myservo.attach(PIN_SERVO);
-  
-  a = 86;
-  b = 306;
 }
 
 float ir_distance(void){
@@ -58,18 +37,12 @@ float ir_distance(void){
 }
 
 int sm_degree(float distance){
-  return 1530 + (300.0 - distance);
-//  if (distance <= 300.0){
-//    return 1520 + (255.0 - distance);
-//  }
-//  else{
-//    return 1520 - (distance - 255.0);
-//  }
+  return 1530 + (245.0 - distance)*0.5;
 }
 
 void loop() {
   float raw_dist = ir_distance();
-  float dist_cali = 100 + 300.0 / (b - a) * (raw_dist - a);
+  float dist_cali = (16*raw_dist + 700)/23;
   Serial.print("min:0,max:500,dist:");
   Serial.print(raw_dist);
   Serial.print(",dist_cali:");
