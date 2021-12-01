e208d2006228bfa6197a927138ac612f88ba1a33
sehyeon518 <84698896+sehyeon518@users.noreply.github.com>
2021-12-01 22:46:56 +0900

    Create 19_example_0.ino

---
+++ 19_example_0.ino
@@ -0,0 +1,29 @@
+#define PIN_IR A0
+#define PIN_LED 9
+
+void setup() {
+// initialize GPIO pins
+  pinMode(PIN_LED,OUTPUT);
+  digitalWrite(PIN_LED, 1);
+  
+  Serial.begin(57600);
+}
+
+float ir_distance(void){ // return value unit: mm
+  float val;
+  float volt = float(analogRead(PIN_IR));
+  val = ((6762.0/(volt-9.0))-4.0) * 10.0;
+  return val;
+}
+
+void loop() {
+  float raw_dist = ir_distance();
+  float dist_cali = (16*raw_dist + 700)/23;
+  Serial.print("min:0,max:500,dist:");
+  Serial.print(raw_dist);
+  Serial.print(",dist_cali:");
+  Serial.println(dist_cali);
+  if(raw_dist > 156 && raw_dist <224) digitalWrite(PIN_LED, 0);
+  else digitalWrite(PIN_LED, 255);
+  delay(20);
+}
